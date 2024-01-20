9834ff48f5a7785b213a2be5213cc02e0af3d5f6
Victor <victornorlanderble@gmail.com>
2024-01-20 10:11:36 +0100

    display products

--- ThePlugShop/src/components/Product.jsx
+++ ThePlugShop/src/components/Product.jsx
@@ -1,6 +1,11 @@
-const Product = () => {
+import { useContext } from "react"
+
+
+
+export const Product = ({product}) => {
+  
+  console.log(product)
   return (
     <div>Product</div>
   )
 }
-export default Product
--- ThePlugShop/src/pages/Home.jsx
+++ ThePlugShop/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { useContext, useEffect } from "react"
 
 // import product context
 import {ProductContext} from '../contexts/ProductContext'
+// import componeneter
+import {Product} from "../components/Product"
 
 const Home = () => {
   // get products from product context
@@ -15,6 +17,7 @@ const Home = () => {
   
 })
 
+
   
 return (
   <div>
@@ -22,10 +25,7 @@ return (
       <div className="container mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-6 ">
         {filteredProducts.map((product) => {
-        return <div className="w-full h-[250px] bg-purple-400" 
-        key={product.id}>{product.title}
-        </div>
-
+        return <Product product={product} key={product.id}/>
       })}</div>
       </div>
     </section>
