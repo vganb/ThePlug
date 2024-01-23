1b363cc81dca6a1657b5a5467a3fa927e45ff46b
Victor <victornorlanderble@gmail.com>
2024-01-23 10:06:46 +0100

    add and remove button cart

--- ThePlugShop/src/components/CartItem.jsx
+++ ThePlugShop/src/components/CartItem.jsx
@@ -2,7 +2,7 @@
 import { Link } from "react-router-dom"
 
 // icons
-import  {IoMdClose} from 'react-icons/io'
+import  {IoMdClose, IoMdRemove, IoMdAdd} from 'react-icons/io'
 export const CartItem = ({item}) => {
   // destructure item
   const {id, title, image, price, amount} = item
@@ -14,18 +14,37 @@ export const CartItem = ({item}) => {
         <img className="max-w-[80px]" src={image} alt="" />
         </Link>
         {/* title and remove icon */}
-        <div>
+        <div className="w-full flex flex-cols">
           <Link to={`/product/${id}`} className="text-sm uppercase 
           font-medium max-w-[240px] 
           text-primary hover:underline">{title}
           </Link>
           {/* remove icon */}
-          <div>
+          <div className="text-xl cursor-pointer">
             <IoMdClose className="text-gray-400 hover:text-red-500
             transition"/>
           </div>
         </div>
       </div>
+          <div className="bg-pink-300 flex gap-x-2 h-[36px] text-sm">
+          {/* qty */}
+          <div className="flex flex-1 max-w-[100px] bg-blue-600 items-center h-full border text-black font-medium">
+            {/* minus icon */}
+            <div className="flex-1 h-full flex justify-center items-center cursor-pointer">
+            <IoMdRemove/>
+            </div>
+            {/* amount */}
+            <div className="h-full flex justify-center items-center px-2">{amount}</div>
+            {/* plus icon */}
+            <div className="flex-1 h-full flex justify-center items-center cursor-pointer">
+            <IoMdAdd/>
+            </div>
+          </div>
+          {/* item price */}
+          <div>item price</div>
+          {/* final price */}
+          <div>final price</div>
+          </div>
     </div>
   )
 }
