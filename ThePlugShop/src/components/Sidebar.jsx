01ad2bc7eae407ece776a5ab63a74cabfb9af86d
Victor <victornorlanderble@gmail.com>
2024-01-24 13:46:18 +0100

    cartcontext error fix

--- ThePlugShop/src/components/Sidebar.jsx
+++ ThePlugShop/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 
 // icons
 import {IoMdArrowForward} from 'react-icons/io'
-// import {Fitrash2} from 'react-icons/fi'
+import {FiTrash2} from 'react-icons/fi'
 
 
 // componenets
@@ -22,7 +22,7 @@ export const Sidebar = () => {
   
   const {isOpen, handleClose }= useContext(SidebarContext)
 
-    const {cart} = useContext(CartContext)
+    const {cart, clearCart} = useContext(CartContext)
 
   return (
     <div className={`${isOpen ? 'right-0' : '-right-full'}  w-full bg-white fixed top-0 h-full shadow-xl md:w-[35vw] xl:max-w 30vw transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
@@ -36,7 +36,20 @@ export const Sidebar = () => {
        <div>
         {cart.map((item) => {
         return <CartItem item={item} key={item.id}/>
-       })}</div>
+       })}
+       </div>
+       <div className="flex flex-col gap-y-3 py-4 mt-4">
+        <div className="flex w-full justify-between items-center">
+        {/* total amount */}
+        <div className="uppercase font-semibold">
+          <span className="mr-2">Total:</span>$1000
+        </div>
+       {/* clear cart icon */}
+          <div onClick={clearCart} className="cursor-pointer py-4 bg-red-400 text-white w-12 h-12 flex text-2xl justify-center items-center">
+            <FiTrash2 />
+            </div>
+          </div>
+       </div>
     </div>
   )
 }
--- ThePlugShop/src/contexts/CartContext.jsx
+++ ThePlugShop/src/contexts/CartContext.jsx
@@ -15,34 +15,34 @@ const CartProvider = ({children}) => {
     const cartItem = cart.find((item) => {
       return item.id === id
     })
-    // if cart item is already in cart
-    if (cartItem) {
-      const newCart =[...cart].map((item) => {
-        if(item.id === id){
-          return { ...item, amount: cartItem.amount + 1}
-        }else {
-          return item
-        }
-      })
-      setCart(newCart)
-    }else {
-      setCart([...cart, newItem])
-    }
-  }
-
-  // remove from cart
-  const removeFromCart = (id) => {
-    const newCart = cart.filter((item) => {
-      return item.id !== id
+   // if cart item is already in cart
+   if (cartItem) {
+    const newCart =[...cart].map((item) => {
+      if(item.id === id){
+        return { ...item, amount: cartItem.amount + 1}
+      }else {
+        return item
+      }
     })
     setCart(newCart)
+  }else {
+    setCart([...cart, newItem])
   }
+}
 
-  
-  return (<CartContext.Provider value={{cart, addToCart, removeFromCart}}>{children}
-    </CartContext.Provider>
-    )
-  }
+// remove from cart
+const removeFromCart = (id) => {
+  const newCart = cart.filter((item) => {
+    return item.id !== id
+  })
+  setCart(newCart)
+}
 
 
-  export default CartProvider
+return (<CartContext.Provider value={{cart, addToCart, removeFromCart}}>{children}
+  </CartContext.Provider>
+  )
+}
+
+
+export default CartProvider
