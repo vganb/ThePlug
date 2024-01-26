4c271ba8aee5666caafa538696deb0725c161030
Victor <victornorlanderble@gmail.com>
2024-01-26 13:37:37 +0100

    checkout and cart button added

--- ThePlugShop/src/components/Sidebar.jsx
+++ ThePlugShop/src/components/Sidebar.jsx
@@ -22,18 +22,18 @@ export const Sidebar = () => {
   
   const {isOpen, handleClose }= useContext(SidebarContext)
 
-    const {cart, clearCart, total} = useContext(CartContext)
+    const {cart, clearCart, total, itemAmount} = useContext(CartContext)
 
   return (
     <div className={`${isOpen ? 'right-0' : '-right-full'}  w-full bg-white fixed top-0 h-full shadow-xl md:w-[35vw] xl:max-w-[35vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
        <div className="flex items-center justify-between py-6 border-b">
-        <div className="uppercase text-md font-semibold">Shopping Cart (0)</div>
+        <div className="uppercase text-md font-semibold">Shopping Cart ({itemAmount})</div>
           <div onClick={handleClose} 
           className="cursor-pointer w-8 h-8 flex justify-center items-center">
             <IoMdArrowForward className='text-2xl' />
           </div>
        </div>
-       <div className="flex flex-col gap-y-2 h-[520px] lg:h-[520px] overflow-y-auto overflow-x-hidden border-b">
+       <div className="flex flex-col gap-y-2 h-[420px] lg:h-[420px] overflow-y-auto overflow-x-hidden border-b">
         {cart.map((item) => {
         return <CartItem item={item} key={item.id}/>
        })}
@@ -49,6 +49,8 @@ export const Sidebar = () => {
             <FiTrash2 />
             </div>
           </div>
+          <Link to={'/'} className="bg-gray-300 flex p-4 justify-center items-center text-black w-full font-medium rounded-xl">View Cart</Link>
+          <Link to={'/'} className="bg-black flex p-4 justify-center items-center text-white w-full font-medium rounded-xl" >Checkout</Link>
        </div>
     </div>
   )
--- ThePlugShop/src/pages/ProductDetails.jsx
+++ ThePlugShop/src/pages/ProductDetails.jsx
@@ -1,6 +1,62 @@
+// import useParams
+import {useParams} from 'react-router-dom'
+
+// import cart context
+import { CartContext } from '../contexts/CartContext'
+
+//  import product context
+import { ProductContext } from '../contexts/ProductContext'
+import { useContext } from 'react'
+
+
 const ProductDetails = () => {
+  // get the product id from url
+  const { id } = useParams()
+  const {products}= useContext(ProductContext)
+  const {addToCart} = useContext(CartContext)
+  
+
+  // get the single product based on id
+
+  const product = products.find(item => {
+    return item.id === parseInt(id)
+  })
+
+// if product is not found
+  if (!product) {
+    return(
+
+    <section className='h-screen flex justify-center items-center'>
+      Loading...
+    </section>
+    )
+  }
+
+  // destructure product
+  const {title, price, description, image} = product
+  
   return (
-    <div>ProductDetailsPage</div>
+    <section className='bg-pink-200 pt-32 pb-12 lg:py-32  h-screen'>
+      <div className="container mx-auto">
+        {/* image och text wrapper */}
+        <div className='flex flex-col lg:flex-row'>
+        {/* image */}
+        <div className='flex flex-1 justify-center items-center
+        mb-8 lg:mb-0'>
+          <img className='max-w-[200px] lg:max-w-sm' src={image} alt="" />
+        </div>
+        {/* text */}
+        <div className='flex-1 text-center lg:text-left'>
+        <h1 className='text-[22px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0'>{title}</h1>
+        {/* price */}
+        <div className='text-xl text-red-700 font-medium mb-6 '>${price}
+        </div>
+        <p className='mb-8 px-6'>{description}</p>
+        <button onClick={() => addToCart(product, product.id)} className='bg-black text-white py-4 px-8 rounded-xl'>Add to cart</button>
+        </div>
+        </div>
+      </div>
+    </section>
   )
 }
 export default ProductDetails
